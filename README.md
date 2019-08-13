f52f2ff80d1349d1256a2758487fee4eeb46f16a
Nathan <nathanmersha@gmail.com>
2019-08-13 14:16:35 +0300

    Added a read me file, and some bug fix

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-gennode_authorization_mw
+# GenNode Authorization Middleware
+
+#### Author
+Nathan Mersha
+
+### Installation
+
+Gennode authorization middleware is available on npm, type:
+
+`$npm i gennode_authorization_mw`
+
+
+### Description
+This module is a middleware to be plugged in your server as described [here](https://www.npmjs.com/package/gennode_authorization) and so the middleware is a supporting library
+to be used along with the other 2 libraries in the cluster. The main function of the middleware is to parse and send request to **GenNode Authorization** server and either responds
+with 401 (unAuthorized) or 200(Authorized) response.
+
+#### How to use it
+
+1. Initialize the middleware with a constructor
+
+```javascript
+let GennodeAuthMW = require('gennode_authorization_mw');
+let GennodeAuthMWInstance = new GennodeAuthMW({
+        host    : "localhost",
+        port    : 3400,
+        endpoint : "/auth/token/validate",
+        connection : "http", // http call, seneca // future seneca support
+        message         : {
+            notAuthorized   : "Access Denied"   // Code 401
+        },
+        authorizationKey : "Authorization", // Defines the key name inside the request header.
+        getAccessObject : this.getAccessObject,
+        getToken        : this.getToken
+    });
+```
+
+| Configuration | Description | Default |
+|:------------:|:-----------:|:-----------:|
+|host               |Gennode Authorization service host | localhost |
+|port               |Gennode Authorization port         | 3400 |
+|endpoint           |Token validator endpoint           | /auth/token/validate |
+|connection         |Communication type (Current support : http) for future seneca support | http |
+|message            |Message for un-authorized responses ( For access granted the middleware calls next(), no need for message| Access Denied |
+|authorizationKey   |Authorization key on the header          | Authorization |
+|getAccessObject    |A function that returns the access object id from the request (Can be overridden)           | ()=> return req.query._id !== undefined ? req.query._id : req.params._id !== undefined ? req.params._id : null; |
+|getToken           |A function that returns the token value from the request (Can be overridden)          | Returns the token value from the 'Bearer tokenValue' by the authorization key from the header|
+
+
+2. Insert the middleware in your express application, like this : 
+
+```javascript
+let express = require('express');
+let app = express();
+
+// Insert your middleware in to your express application
+app.use(GennodeAuthMWInstance.authorize);
+
+```
+
+### Contributing
+**If you have anything in mind, that you think is would be awesome to include in the generated server files, feel free to create an issue [here](https://github.com/nathan-mersha/gennode_authorization_mw), 
+or fork the project.**
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /**
+ * @author              - Nathan Mersha
+ * @copyright           - August 2019
  * @name                - Gennode Authorization Middleware
  * @description         - Middle ware for gennode authorization service
- * @param req           - Request Object
- * @param res           - Response Object
- * @param next          - Next
  */
 
 
@@ -15,42 +14,54 @@ class GennodeAuthorizationMiddleware {
      * @param userConfig        - User configuration
      * @example
      * {
-            authEndPoint    : "http://localhost:3400/auth/token/validate",
-            message         : {
+            host            : "localhost",
+            port             : 3400,
+            endpoint         : "/auth/token/validate",
+            connection       : "http", // http call, seneca // todo future seneca support
+            message          : {
                 authorized      : "Access Granted", // Code 401
                 notAuthorized   : "Access Denied"   // Code 200
             },
-            accessObject    : {
-                body : ["_id","id"],
-                url  : ["_id","id"]
-            }
-        }
+            authorizationKey : "Authorization", // Defines the key name inside the request header.
+            getAccessObject  : this.getAccessObject,
+            getToken         : this.getToken
+       }
      */
     constructor(userConfig){
+        // Binding methods
+        this.mergeConfig = this.mergeConfig.bind(this);
+        this.sendRequest = this.sendRequest.bind(this);
+        this.authorize = this.authorize.bind(this);
+        this.getAccessObject = this.getAccessObject.bind(this);
+        this.getToken = this.getToken.bind(this);
 
         this.async      = require('async');
         this.debug      = require('debug')('Gennode-Authorization-Middleware');
         this.request    = require('request');
         this.xtend      = require('xtend');
-        this.errorCodes = require('./lib/constant').errorCodes;
+        this.exec       = require('child_process').exec;
+        this.errorCodes = require('./lib/constant/errorCodes');
 
         // Define default configuration
         this.defaultConfig = {
-            authEndPoint    : "http://localhost:3400/auth/token/validate",
+            host    : "localhost",
+            port    : 3400,
+            endpoint : "/auth/token/validate",
+            connection : "http", // http call, seneca // todo future seneca support
             message         : {
-                authorized      : "Access Granted", // Code 401
-                notAuthorized   : "Access Denied"   // Code 200
+                notAuthorized   : "Access Denied"   // Code 401
             },
+            authorizationKey : "Authorization", // Defines the key name inside the request header.
             getAccessObject : this.getAccessObject,
-            getToken        : this.getToken,
+            getToken        : this.getToken
         };
 
         // Sample request object to test func() return value.
         const sampleRequest = {
             query   : {_id : "someId"},
             params  : {_id : "someId"},
-            get     : function (requestedHeader) {
-                if(requestedHeader === "Authorization"){
+            get     : (requestedHeader) =>{
+                if(requestedHeader === this.mergedConfig.authorizationKey){
                     return "Bearer someTokenValues";
                 }else{
                     return "something else";
@@ -81,6 +92,15 @@ class GennodeAuthorizationMiddleware {
             throw new Error("getToken() must be a function, with a return value that is not undefined.");
         }
 
+        // ping authorization server and log status
+        this.exec(`ping -c 3 ${this.mergedConfig.host.toString().replace("http://", "").replace("https://", "")}`, (error, stdout, stderr)=> {
+            error || stderr ? this.debug(error, '\n\n', stderr, '\n', `Your authorization server may not be up at ${this.mergedConfig.host}`) : this.debug(stdout);
+        });
+
+        // Binding these methods after configuration merge is completed.
+        this.getAccessObject = this.getAccessObject.bind(this);
+        this.getToken = this.getToken.bind(this);
+
     }
 
     /**
@@ -91,9 +111,11 @@ class GennodeAuthorizationMiddleware {
      * @returns {*}             - Merged Configuration
      */
     mergeConfig(defaultConfig, userConfig){
-
         let mergedConfig = this.xtend(defaultConfig, userConfig);
-        mergedConfig.message = this.xtend(defaultConfig.message, userConfig.message);
+        // Make message merge config if user has provided config message values.
+        if(userConfig !== undefined){
+            mergedConfig.message = this.xtend(defaultConfig.message, userConfig.message);
+        }
         return mergedConfig;
     }
 
@@ -104,7 +126,6 @@ class GennodeAuthorizationMiddleware {
      * @returns {*}             - Either _id on query on param. (Method could be overridden with any return method)
      */
     getAccessObject(req){
-
         return req.query._id !== undefined ? req.query._id : req.params._id !== undefined ? req.params._id : null;
     }
 
@@ -117,25 +138,64 @@ class GennodeAuthorizationMiddleware {
      * @returns *               - Token value
      */
     getToken(req, res){
-
-        let token = req.get("Authorization").split(" ");
-        let errMsg = this.errorCodes.AUT.AUTHENTICATION_TYPE_NOT_ACCORD;
-
-        // Checking token type
-        if(token[0] !== "Bearer"){
-            errMsg.detail = "Token type must be 'Bearer'";
+        let errorCodes  = require('./lib/constant/errorCodes');
+        // Checking if the request header contains the appropriate key word as defined by the configuration, default key word is
+        if(! req.get(this.mergedConfig.authorizationKey)){
+            let errMsg = errorCodes.AUT.AUTHENTICATION_TYPE_NOT_ACCORD;
+            errMsg.detail = `Request header must contain an authorization key word : ${this.mergedConfig.authorizationKey}`;
             res.status(401);
             res.json(errMsg);
-        }
+        }else{
+            let
+                token       = req.get(this.mergedConfig.authorizationKey).split(" "),
+                errMsg      = errorCodes.AUT.AUTHENTICATION_TYPE_NOT_ACCORD;
 
-        // Checking token format
-        if(token.length !== 2){
-            errMsg.detail = "Token must have the format 'Bearer tokenValue' (Note : there is a space between the token type and the value)";
-            res.status(401);
-            res.json(errMsg);
-        }
+            // Checking token type
+            if(token[0] !== "Bearer"){
+                errMsg.detail = "Token type must be 'Bearer'";
+                res.status(401);
+                res.json(errMsg);
+            }
 
-        return token[1]; // Return token value
+            // Checking token format
+            if(token.length !== 2){
+                errMsg.detail = "Token must have the format 'Bearer tokenValue' (Note : there is a space between the token type and the value)";
+                res.status(401);
+                res.json(errMsg);
+            }
+            return token[1]; // Return token value
+        }
+    }
+
+    /**
+     * @name                - Send request
+     * @description         - Sends request
+     * @param body          - Body to send
+     * @param method        - Http Method
+     * @param endPoint      - Endpoint
+     * @param callback      - Callback function (error, response, body)
+     */
+    sendRequest(body, method, endPoint, callback){
+
+        let options = option(method,body); // defines sending options
+        this.request(endPoint,options,function (err,res,body) {
+            callback(err,res,body)
+        });
+
+        /**
+         * @name                - Option
+         * @description         - Constructs option object
+         * @param method        - Http Method
+         * @param body          - Request body
+         * @returns {{method: *, json: boolean, body: *}}
+         */
+        function option           (method,body)    {
+            return {
+                method : method, // defines the method PUT,GET,DELETE,REMOVE
+                json : (body !== null), // defines true only when body data is available to attach
+                body : body
+            }
+        }
     }
 
     /**
@@ -146,22 +206,31 @@ class GennodeAuthorizationMiddleware {
      * @param next              - Next
      */
     authorize(req,res,next){
+        let
+            objectId = this.mergedConfig.getAccessObject(req),
+            token    = this.mergedConfig.getToken(req,res);
 
-        let authorizeBody = {
+        let body = {
             route       : req.url,
             method      : req.method,
-            body        : req.body,
-            objectId    : this.mergedConfig.getAccessObject(req),
-            token       : this.mergedConfig.getToken(req, res)
+            body        : !req.body ? null : req.body,
+            objectId    : objectId,
+            token       : token
         };
 
-
+        if(token){ // Perform authorization request if token exists.
+            let constructedEndpoint = `http://${this.mergedConfig.host}${this.mergedConfig.port ? `:${this.mergedConfig.port}` : ""}${this.mergedConfig.endpoint}`;
+            this.sendRequest(body,"POST",constructedEndpoint,(error, response, body) =>{
+                if(response.statusCode === 200){ // Request of the resource is authorized.
+                    next();
+                }else {
+                    body.detail = this.mergedConfig.message.notAuthorized;
+                    res.status(response.statusCode);
+                    res.json(body);
+                }
+            });
+        }
     }
-
-
 }
 
-
-
 module.exports = GennodeAuthorizationMiddleware;
-
